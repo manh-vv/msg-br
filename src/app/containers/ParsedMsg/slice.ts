75237ba4896ef3c9350d9ecf333a76346b3bcb95
manh <manh.vv.htth@gmail.com>
2021-01-24 20:07:59 +0700

    feat: :sparkles: spell number

--- src/app/containers/ParsedMsg/slice.ts
+++ src/app/containers/ParsedMsg/slice.ts
@@ -1,4 +1,5 @@
 import { PayloadAction } from '@reduxjs/toolkit';
+import { MsgPart } from 'types/MsgPart';
 import { MsgPartContainer } from 'types/MsgPartContainer';
 import { createSlice } from 'utils/@reduxjs/toolkit';
 import { ContainerState } from './types';
@@ -16,8 +17,28 @@ const parsedMsgSlice = createSlice({
   name: 'parsedMsg',
   initialState,
   reducers: {
+    parseMessage(_, _action: PayloadAction<{ line: string; idx: number }>) {},
+    spellNumber(_, _action: PayloadAction<{ line: string; idx: number }>) {},
     parsedMsg(state, action: PayloadAction<MsgPartContainer>) {
       state.parsedMsg = action.payload;
+    },
+    addMsgPart(state, action: PayloadAction<{ msgPart: MsgPart; idx: number }>) {
+      const { msgPart, idx } = action.payload;
+
+      state.parsedMsg.msgParts = [
+        ...state.parsedMsg.msgParts.slice(0, idx),
+        msgPart,
+        ...state.parsedMsg.msgParts.slice(idx + 1),
+      ];
+
+      if (msgPart.parts.length > state.parsedMsg.maxCol) {
+        state.parsedMsg.maxCol = msgPart.parts.length;
+      }
+
+      msgPart.posVParts = msgPart.parts.filter(p => p.isPos || p.type === 'number');
+      if (msgPart.posVParts.length > state.parsedMsg.maxPosCol) {
+        state.parsedMsg.maxPosCol = msgPart.posVParts.length;
+      }
     },
   },
 });
